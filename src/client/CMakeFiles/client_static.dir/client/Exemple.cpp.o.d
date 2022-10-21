aedf6f4439678c0970bd1f126682d768bbed54cc
AliZhr <93094424+AliZhr@users.noreply.github.com>
2022-10-21 23:41:30 +0200

    Add files via upload

---
+++ src/client/CMakeFiles/client_static.dir/client/Exemple.cpp.o.d
@@ -0,0 +1,5 @@
+src/client/CMakeFiles/client_static.dir/client/Exemple.cpp.o: \
+ /home/ali/Bureau/plt/src/client/client/Exemple.cpp \
+ /usr/include/stdc-predef.h \
+ /home/ali/Bureau/plt/src/shared/state/Exemple.h \
+ /home/ali/Bureau/plt/src/client/client/Exemple.h
---
+++ test/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/ali/Bureau/plt/test
+# Build directory: /home/ali/Bureau/plt/build/test
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("shared")
+subdirs("client")
