1f756b42245dbcd6580af6e3813b92f13e0e8737
AliZhr <93094424+AliZhr@users.noreply.github.com>
2022-10-21 23:40:05 +0200

    Add files via upload

---
+++ run_test.sh
@@ -0,0 +1,2 @@
+cd "/home/ali/Bureau/plt/build/test"
+ctest --timeout 300 ||/bin/true
---
+++ src/shared/CMakeFiles/shared_static.dir/state/Exemple.cpp.o.d
@@ -0,0 +1,4 @@
+src/shared/CMakeFiles/shared_static.dir/state/Exemple.cpp.o: \
+ /home/ali/Bureau/plt/src/shared/state/Exemple.cpp \
+ /usr/include/stdc-predef.h \
+ /home/ali/Bureau/plt/src/shared/state/Exemple.h
