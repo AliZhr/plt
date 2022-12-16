7fdaa012af2b0b7455d6fb8cd59a46a3fe707bb8
Lavan <80328975+Senakyw@users.noreply.github.com>
2022-12-16 08:40:11 +0100

    Delete cmake-build-debug directory

--- cmake-build-debug/test/shared/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/lavansri/ENSEA/Hive_IA/plt/test/shared
-# Build directory: /home/lavansri/ENSEA/Hive_IA/plt/cmake-build-debug/test/shared
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(test_shared_dummy "/home/lavansri/ENSEA/Hive_IA/plt/cmake-build-debug/test/shared/test_shared_dummy")
-set_tests_properties(test_shared_dummy PROPERTIES  WORKING_DIRECTORY "/home/lavansri/ENSEA/Hive_IA/plt" _BACKTRACE_TRIPLES "/home/lavansri/ENSEA/Hive_IA/plt/test/CMakeLists.txt;77;add_test;/home/lavansri/ENSEA/Hive_IA/plt/test/shared/CMakeLists.txt;2;add_custom_test;/home/lavansri/ENSEA/Hive_IA/plt/test/shared/CMakeLists.txt;0;")
