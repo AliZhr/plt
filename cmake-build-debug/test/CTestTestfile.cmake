193e3283d2042c742ff56113d42cf8efc174023c
Lavan <lavan4skr@gmail.com>
2022-12-16 08:43:14 +0100

    feat : engine with render

--- cmake-build-debug/test/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/lavansri/ENSEA/Hive_IA/plt/test
-# Build directory: /home/lavansri/ENSEA/Hive_IA/plt/cmake-build-debug/test
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("shared")
-subdirs("client")
--- cmake-build-debug/test/client/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/lavansri/ENSEA/Hive_IA/plt/test/client
-# Build directory: /home/lavansri/ENSEA/Hive_IA/plt/cmake-build-debug/test/client
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(test_client_dummy "/home/lavansri/ENSEA/Hive_IA/plt/cmake-build-debug/test/client/test_client_dummy")
-set_tests_properties(test_client_dummy PROPERTIES  WORKING_DIRECTORY "/home/lavansri/ENSEA/Hive_IA/plt" _BACKTRACE_TRIPLES "/home/lavansri/ENSEA/Hive_IA/plt/test/CMakeLists.txt;77;add_test;/home/lavansri/ENSEA/Hive_IA/plt/test/client/CMakeLists.txt;3;add_custom_test;/home/lavansri/ENSEA/Hive_IA/plt/test/client/CMakeLists.txt;0;")
